570b00ce3207362e19df78a7fb4bb092b5a590cc
Charles Davis <tgcd5@me.com>
2018-12-03 18:07:15 -0500

    Initial Commit

---
+++ Treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treads
+//
+//  Created by gdm on 12/3/18.
+//  Copyright © 2018 gdm. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
