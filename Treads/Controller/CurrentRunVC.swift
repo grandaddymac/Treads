2382e2e9b7b6e7b8fa9873f6515f9bbd48846b32
gdm <gdm.mbp@gdmMBP.local>
2018-12-04 16:07:43 -0500

    Add location tracking logic

--- Treads/Controller/CurrentRunVC.swift
+++ Treads/Controller/CurrentRunVC.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import MapKit
 
 class CurrentRunVC: LocationVC {
 
     @IBOutlet weak var swipeBGImageView: UIImageView!
     
     @IBOutlet weak var sliderImageView: UIImageView!
+    
+    @IBOutlet weak var durationLabel: UILabel!
+    
+    @IBOutlet weak var paceLabel: UILabel!
+    
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    @IBOutlet weak var pauseButton: UIButton!
+    
+    var startLocation: CLLocation!
+    var lastLocation: CLLocation!
+    var runDistance = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +35,23 @@ class CurrentRunVC: LocationVC {
         sliderImageView.isUserInteractionEnabled = true
         swipeGesture.delegate = self as? UIGestureRecognizerDelegate
         
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        manager?.delegate = self
+        manager?.distanceFilter = 10
+        startRun()
+    }
+    
+    func startRun() {
+        manager?.startUpdatingLocation()
+    }
+    
+    func endRun() {
+        manager?.stopUpdatingLocation()
+    }
+    
+    @IBAction func pauseButtonPressed(_ sender: Any) {
     }
     
     @objc func endRunSwiped(sender: UIPanGestureRecognizer) {
@@ -49,4 +79,22 @@ class CurrentRunVC: LocationVC {
     }
    
 
+}
+
+extension CurrentRunVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            checkLocationAuthStatus()
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if startLocation == nil {
+            startLocation = locations.first
+        } else if let location = locations.last {
+            runDistance += lastLocation.distance(from: location)
+            distanceLabel.text = "\(runDistance)"
+        }
+            lastLocation = locations.last
+        
+    }
 }
